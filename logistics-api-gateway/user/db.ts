96d18d2058f2cfa5797d1f9229a35feac117443d
Kottage N.V.H <nowendevelopment@gmail.com>
2024-04-08 23:35:34 +0530

    feat: logistics addded

---
+++ logistics-api-gateway/user/db.ts
@@ -0,0 +1,24 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is not defined");
+}
+
+const db = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Database Error " + error);
+  }
+};
+
+export default db;
