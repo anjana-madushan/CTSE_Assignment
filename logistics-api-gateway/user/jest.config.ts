50fb0f008b6c1b3ed34b4a26621a82100ef34349
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2024-04-20 13:29:09 +0530

    jest configuration + tsconfig for all services

---
+++ logistics-api-gateway/user/jest.config.ts
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  coverageDirectory: './__tests__',
+  rootDir: './',
+  testEnvironment: 'node',
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "<rootDir>/server.ts"
+  ],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js|jsx)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ["<rootDir>/node_modules"]
+};
